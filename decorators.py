4a4bd3683ce6e6e23a997848b525eb239d600b89
momojohnson <momo.johnson1987@gmail.com>
2018-05-29 23:06:19 -0500

    Added comment to decorator.py file

--- decorators.py
+++ decorators.py
@@ -4,6 +4,10 @@ from flask import flash, redirect, url_for
 from flask_login import current_user
 
 def check_confirmed(func):
+    """
+    A decorator that checks to ensure that a user
+    has confirmed their email before they can have access to the application
+    """
     @wraps(func)
     def decorated_function(*args, **kwargs):
         if current_user.confirm_email is False:
